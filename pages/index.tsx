d55eea35f6a31fcb187b80bfd42e8e8ddfddd37f
yashgadbail <yashgadbail999@gmail.com>
2025-04-06 14:19:06 +0530

    build-5

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,11 @@ import Hero from "../components/Hero";
 import Features from "../components/Features";
 import styles from "../styles/Home.module.css";
 import { prefix } from "../utils/prefix";
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>PhysicsNotes - Home</title>
         <meta name="description" content="Comprehensive physics learning platform" />
@@ -16,16 +17,27 @@ const Home: NextPage = () => {
         <link rel="icon" href={`${prefix}/favicon.ico`} />
       </Head>
 
-      <Navbar />
-      <Hero />
-      <Features />
+      <div className={styles.container}>
+        <nav className={styles.nav}>
+          <Link href="/physicsnotes" className={styles.navLink}>Home</Link>
+          <Link href="/physicsnotes/notes" className={styles.navLink}>Notes</Link>
+          <Link href="/physicsnotes/dpp" className={styles.navLink}>DPP</Link>
+          <Link href="/physicsnotes/blogs" className={styles.navLink}>Blogs</Link>
+          <Link href="/physicsnotes/formula-handbook" className={styles.navLink}>Formulas</Link>
+          <Link href="/physicsnotes/exam-preparation" className={styles.navLink}>Exam Prep</Link>
+          <Link href="/physicsnotes/login" className={styles.navLink}>Login</Link>
+        </nav>
+        <Navbar />
+        <Hero />
+        <Features />
 
-      <footer className={styles.footer}>
-        <p>
-          PhysicsNotes - Your Complete Guide to Class 11 & 12 Physics
-        </p>
-      </footer>
-    </div>
+        <footer className={styles.footer}>
+          <p>
+            PhysicsNotes - Your Complete Guide to Class 11 & 12 Physics
+          </p>
+        </footer>
+      </div>
+    </>
   );
 };
 
